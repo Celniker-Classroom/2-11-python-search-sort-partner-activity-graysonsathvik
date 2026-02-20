cf01648946cbdf79e738cbaf38dfd6d1d7f02a5c
SathvikRox <sethuramans5817@my.sduhsd.net>
2026-02-20 17:10:07 +0000

    binarysearch

---
+++ binarysearch.py
@@ -0,0 +1,24 @@
+from random import randint #This line imports the randint function from the random module. The randint function generates a random integer between two specified values
+
+
+ranNums = [] #name your list and make sure it is empty!
+
+length = int(input("How long do you want the list to be? "))
+# Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
+for i in range(length): #for loop appends "length" numbers to your list, but make sure you name your variable
+    ranNums.append(randint(1,50)) #this adds a random number between 1-50 to the list
+ranSearch = input("What number do you want to search for?")
+print(ranNums)
+print("You are searching for", ranSearch)
+
+comparisons = 0  # Initialize the counter for comparisons
+found = False  # Variable to track if the number was found
+
+
+for count in ranNums:  # Name your variable in the for loop
+    comparisons += 1  # Increment the counter for each comparison
+    if count == int(ranSearch):
+        found = True  # Set found to True if the number is in the list
+        break  # Exit the loop early if the number is found
+print(f"The number of comparisons is {comparisons}")
+print(found)
